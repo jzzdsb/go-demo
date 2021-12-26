ebf084fcf992491f59c6c94d5ce677815ce04f51
jzzdsb <jzz835512239@gmail.com>
2021-12-26 21:31:08 +0800

    去除强转

--- arithmetic/dp.go
+++ arithmetic/dp.go
@@ -1,14 +1,12 @@
 /*
  * @Author: vic
  * @Date: 2021-12-26 18:40:59
- * @LastEditTime: 2021-12-26 21:01:42
+ * @LastEditTime: 2021-12-26 21:28:29
  * @LastEditors: Please set LastEditors
  * @Description: 简单的算法
  * @FilePath: /src/demo/arithmetic/dp.go
  */
 package arithmetic
-
-import "math"
 
 /**
  * @description:
@@ -22,18 +20,24 @@ func BasePackage(w []int, v []int, target int) int {
 	//物品数
 	n := len(w)
 
-	//重量
-	dp := make([]float64, target+1)
+	//重量 Math.max(int,int),太简单，所以要用户自己实现，笑死我了
+	dp := make([]int, target+1)
 
 	for i := 0; i < n; i++ {
 		for j := target; j > 0; j-- {
 			if j-w[i] >= 0 {
-				dp[j] = math.Max(dp[j-1], dp[j-w[i]]+float64(v[i]))
+				//go没有三元运算符
+				if (dp[j-1] - (dp[j-w[i]] + v[i])) > 0 {
+					dp[j] = dp[j-1]
+				} else {
+					dp[j] = dp[j-w[i]] + v[i]
+				}
+
 			} else {
 				dp[j] = dp[j-1]
 			}
 		}
 	}
 
-	return int(dp[target])
+	return dp[target]
 }
