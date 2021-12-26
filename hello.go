0ac77d17faf8918fc9889ad3fddf1d7ffb64e6bd
jzzdsb <jzz835512239@gmail.com>
2021-12-26 13:45:44 +0800

    测试vscode提交

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	//引入自己的包
+	"demo/constant"
+)
 
 func main() {
-	fmt.Println("hello world")
+	//fmt.Println("hello World")
+	fmt.Println(constant.NAME)
+
+	var balance = [5]float32{1000.0, 2.0, 3.4, 7.0, 50.0}
+
+	for _,x:=range balance{
+		fmt.Println(x)
+	}
 }
